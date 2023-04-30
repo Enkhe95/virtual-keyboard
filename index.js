dd66eb63155051386434b900f7a341bccd24de79
Enkhe Garmaev <garmaevenhe@gmail.com>
2023-04-30 16:22:06 +0800

    feat: add tag <textarea>

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const title = document.createElement("h1");
 title.setAttribute("class", "title");
 title.innerHTML = "Virtual Keyboard";
 
+const textarea = document.createElement("textarea");
+textarea.setAttribute("class", "textarea");
+
 const body = document.getElementById("root");
 body.appendChild(title);
+body.appendChild(textarea);
