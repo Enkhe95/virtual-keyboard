20f85fc51bb1b75b399c430ddfabcca54f297888
Enkhe Garmaev <garmaevenhe@gmail.com>
2023-04-30 15:43:50 +0800

    feat: add title

---
+++ index.js
@@ -0,0 +1,6 @@
+const title = document.createElement("h1");
+title.setAttribute("class", "title");
+title.innerHTML = "Virtual Keyboard";
+
+const body = document.getElementById("root");
+body.appendChild(title);
